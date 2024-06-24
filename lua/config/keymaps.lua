8e9cddb03c47982469eb166a27cbfd5cd7ffe22b
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-06-24 22:48:48 +0300

    Making bg transparent and adding keybinds

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Select the whole file with Ctrl + a in normal mode
+map("n", "<C-a>", "ggVG", { noremap = true, silent = true })
+
+-- Exit insert mode with 'jj' in insert mode
+map("i", "jj", "<Esc>", { noremap = true, silent = true })
