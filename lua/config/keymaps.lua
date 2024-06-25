e3e3ea773eb4bb4898f8487cadaef59536f64346
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-06-25 04:39:50 +0300

    Adding multi cursors and changing the dashboard and adding keybind for search and replace using Regex

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 
 -- Select the whole file with Ctrl + a in normal mode
 map("n", "<C-a>", "ggVG", { noremap = true, silent = true })
 
 -- Exit insert mode with 'jj' in insert mode
 map("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+map("n", "zz", ":'<,'>s//<Left>", { noremap = true, silent = true })
