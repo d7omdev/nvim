ab5e3a0b4a0256112601368ba72b6b83eb9f6021
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-06-25 04:48:45 +0300

    Used smoka7/multicursors.nvim for multi Cursor
    used smoka7/multicursors.nvim instade of Vim-visual-multi

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -1,5 +1,17 @@
 return {
-  "mg979/vim-visual-multi",
-  lazy = false,
-  keys = {},
+  "smoka7/multicursors.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "smoka7/hydra.nvim",
+  },
+  opts = {},
+  cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+  keys = {
+    {
+      mode = { "v", "n" },
+      "<Leader>m",
+      "<cmd>MCstart<cr>",
+      desc = "Multi cursors",
+    },
+  },
 }
