68eecb377dbf4016e8dfb051a4426454ee4e7bcc
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-06-27 18:33:15 +0300

    fix bg color problems

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		opts = {
-			transparent_background = true,
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
-	{
-		-- "DaikyXendo/nvim-material-icon",
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      flavor = "mocha",
+      background = {
+        dark = "frappe",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    -- "DaikyXendo/nvim-material-icon",
+  },
 }
