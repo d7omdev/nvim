932bbf352c780b3ea31f7c03a40e7d38ad70fda3
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-06-28 05:34:15 +0300

    used rainbow-delimiters other than nvim-ts-rainbow

--- lua/plugins/image-nvim.lua
+++ lua/plugins/image-nvim.lua
@@ -1,15 +1,10 @@
 return {
   {
-    -- luarocks.nvim is a Neovim plugin designed to streamline the installation
-    -- of luarocks packages directly within Neovim. It simplifies the process
-    -- of managing Lua dependencies, ensuring a hassle-free experience for
-    -- Neovim users.
-    -- https://github.com/vhyrro/luarocks.nvim
     "vhyrro/luarocks.nvim",
-    -- this plugin needs to run before anything else
     priority = 1001,
     opts = {
-      rocks = { "magick" },
+      rocks = {}, -- Remove 'magick' from here
+      hererocks = true,
     },
   },
   {
@@ -19,6 +14,10 @@ return {
       require("image").setup({
         backend = "kitty",
         kitty_method = "normal",
+        lua_version = "5.4",
+        rocks = {
+          hererocks = true,
+        },
         integrations = {
           -- Notice these are the settings for markdown files
           markdown = {
