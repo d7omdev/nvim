8049f83bdab5f02518b93c006ea94cc0f13beaac
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-07-04 17:02:48 +0300

    modefied

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -17,7 +17,7 @@ return {
         cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
       },
       trigger_events = { -- See :h events
-        immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+        immediate_save = { "BufLeave" }, -- vim events that trigger an immediate save
         defer_save = { "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
         cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
       },
