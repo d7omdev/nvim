253dec84e912e73402844510000cb26635d3044a
abdulrahmanDev1 <abdohamdy198461@gmail.com>
2024-07-21 18:42:30 +0300

    added windows.nvim

---
+++ lua/plugins/windows.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "anuvyklack/middleclass",
+    "anuvyklack/animation.nvim",
+  },
+  {
+    "anuvyklack/windows.nvim",
+    requires = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      vim.o.winwidth = 10
+      vim.o.winminwidth = 10
+      vim.o.equalalways = false
+      require("windows").setup()
+    end,
+  },
+}
