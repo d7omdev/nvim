c735d56e73bfa15a84f448cdb0b92dc37a1ac1c7
d7omdev <abdohamdy198461@gmail.com>
2024-11-21 04:59:41 +0300

    minor fixes

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,12 +5,13 @@ return {
       diagnostics = {
         underline = true,
         update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "■",
-          -- prefix = "icons",
-        },
+        -- virtual_text = {
+        --   spacing = 4,
+        --   source = "if_many",
+        --   prefix = "■",
+        --   -- prefix = "icons",
+        -- },
+        virtual_text = false,
         severity_sort = true,
         signs = {
           text = {
