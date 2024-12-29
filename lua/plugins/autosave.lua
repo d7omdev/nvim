3fcdf23720ad4754a8b3521decd3a2954c53130f
d7omdev <abdohamdy198461@gmail.com>
2024-12-30 02:04:58 +0300

    chnged delay time for autosave

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -6,7 +6,7 @@ return {
     opts = {
       enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
       trigger_events = { -- See :h events
-        immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+        immediate_save = { "BufLeave" }, -- vim events that trigger an immediate save
         defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
         cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
       },
@@ -18,7 +18,7 @@ return {
       write_all_buffers = false, -- write all buffers when the current one meets `condition`
       noautocmd = false, -- do not execute autocmds when saving
       lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
-      debounce_delay = 1000, -- delay after which a pending save is executed
+      debounce_delay = 5000, -- delay after which a pending save is executed
       -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
       debug = false,
     },
